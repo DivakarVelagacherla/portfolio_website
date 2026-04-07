89f878fadb437608224c45316221e97ed5718e64
Divakar Velagacherla <divakarvelagacherla@gmail.com>
2026-04-07 18:47:17 -0400

    Router added for home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingHeader } from './shared/landing-header/landing-header';
-import { TitleSection } from './core/title-section/title-section';
+import { TitleSection } from './shared/title-section/title-section';
 import { CurrentlyCard } from './shared/currently-card/currently-card';
 import { QuoteSection } from './core/quote-section/quote-section';
 import { CtaSection } from './shared/cta-section/cta-section';
