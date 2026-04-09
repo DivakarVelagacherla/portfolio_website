72992a165e4fbd02e8eeb1a84560fd2488f40c6c
Divakar Velagacherla <divakarvelagacherla@gmail.com>
2026-04-09 02:27:27 -0400

    yet to build dashboard

--- src/app/pages/engineering/engineering.ts
+++ src/app/pages/engineering/engineering.ts
@@ -3,10 +3,11 @@ import { TitleSection } from '../../shared/title-section/title-section';
 import { ThemeService } from '../../shared/theme.service';
 import { QuoteEngineering } from '../../core/quote-engineering/quote-engineering';
 import { EngineeringSummary } from '../../core/engineering-summary/engineering-summary';
+import { Dashboard } from '../../core/dashboard/dashboard';
 
 @Component({
   selector: 'app-engineering',
-  imports: [TitleSection, QuoteEngineering, EngineeringSummary],
+  imports: [TitleSection, QuoteEngineering, EngineeringSummary, Dashboard],
   templateUrl: './engineering.html',
   styleUrl: './engineering.css',
 })
